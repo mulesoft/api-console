9b0b9123909a2cdf2c441ede2399366b47021f3f
Alejo Fernandez <alejofernandez@me.com>
2013-07-16 18:28:22 -0700

    Added conditional display to additional parameters section

--- angular-app/app/scripts/controllers/raml-operation-details-try-it.js
+++ angular-app/app/scripts/controllers/raml-operation-details-try-it.js
@@ -2,6 +2,6 @@ angular.module('ramlConsoleApp')
     .controller('ramlOperationDetailsTryIt', function ($scope, ramlHelper) {
         $scope.urlParams = ramlHelper.processUrlParts($scope.resource.relativeUri);
         $scope.hasAdditionalParams = function (operation) {
-            return (operation.query && operation.query.length) || operation.method == 'POST' || operation.method == 'PUT'
+            return (operation.query && operation.query.length) || operation.method === 'post' || operation.method === 'put';
         }
     });
