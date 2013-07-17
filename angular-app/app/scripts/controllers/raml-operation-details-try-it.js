fa7bf6e79cfeedb48e6c63cf37182f08675e4e02
Alejo Fernandez <alejofernandez@me.com>
2013-07-17 11:01:10 -0700

    Added Query Params

--- angular-app/app/scripts/controllers/raml-operation-details-try-it.js
+++ angular-app/app/scripts/controllers/raml-operation-details-try-it.js
@@ -1,13 +1,15 @@
 angular.module('ramlConsoleApp')
-    .controller('ramlOperationDetailsTryIt', function ($scope, ramlHelper) {
+    .controller('ramlOperationDetailsTryIt', function ($scope) {
         $scope.request = {};
         $scope.url = {};
-
-        $scope.urlParams = ramlHelper.processUrlParts($scope.resource.relativeUri);
-        $scope.queryParams = ramlHelper.processQueryParts($scope.operation.query);
+        $scope.query = {};
 
         $scope.hasAdditionalParams = function (operation) {
             return operation.query || operation.method === 'post' || operation.method === 'put';
+        };
+
+        $scope.hasRequestBody = function (operation) {
+            return operation.method === 'post' || operation.method === 'put';
         };
 
         $scope.showResponse = function () {
