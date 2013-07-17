1cc8e9997df3395b20caf8048c12f1b0981838ba
Alejo Fernandez <alejofernandez@me.com>
2013-07-16 18:22:35 -0700

    Added method selection handler

--- angular-app/app/scripts/controllers/raml-operation.js
+++ angular-app/app/scripts/controllers/raml-operation.js
@@ -1,10 +1,19 @@
 angular.module('ramlConsoleApp')
-    .controller('ramlOperation', function ($scope) {
+    .controller('ramlOperation', function ($scope, $filter) {
         if ($scope.resource.methods.length) {
             $scope.operation = $scope.resource.methods[0];
         }
         $scope.headerClick = function () {
             this.toggle('active');
+        };
+        $scope.changeMethod = function (methodName) {
+            var method = $filter('filter')(this.resource.methods, { method: methodName });
+            if (method && method.length) {
+                $scope.operation = method[0];
+            }
+        };
+        $scope.isMethodActive = function (methodName) {
+            return this.operation && (this.operation.method === methodName)
         };
         $scope.toggle = function (member) {
             this[member] = !this[member];
