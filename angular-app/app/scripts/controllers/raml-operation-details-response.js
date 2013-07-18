312a14f4117c6b30da20d16da7d6e4e18f29d813
Javier Centurion <javier.centurion@mulesoft.com>
2013-07-18 02:21:20 -0300

    added response details

---
+++ angular-app/app/scripts/controllers/raml-operation-details-response.js
@@ -0,0 +1,24 @@
+angular.module('ramlConsoleApp')
+    .controller('ramlOperationDetailsResponse', function ($scope, $filter) {
+        var statusCodes = [],
+            methodDescriptor = $filter('filter')($scope.resource.methods, {
+                method: $scope.operation.method
+            })[0];
+
+        if (methodDescriptor.responses) {
+            for (var prop in methodDescriptor.responses) {
+                var response = methodDescriptor.responses[prop],
+                    example = response.body ? response.body['application/json'].example : '',
+                    schema = response.body ? response.body['application/json'].schema : '';
+
+                statusCodes.push({
+                    name: prop,
+                    description: response.description,
+                    example: example,
+                    schema: schema
+                });
+            }
+        }
+
+        $scope.statusCodes = statusCodes;
+    });
