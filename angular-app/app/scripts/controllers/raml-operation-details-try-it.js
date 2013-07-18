0298135728179ba31f323fb248d266b3a4ea7b6f
Alejo Fernandez <alejofernandez@me.com>
2013-07-18 13:00:08 -0700

    Some refactors for Try-It

--- angular-app/app/scripts/controllers/raml-operation-details-try-it.js
+++ angular-app/app/scripts/controllers/raml-operation-details-try-it.js
@@ -9,7 +9,7 @@ angular.module('ramlConsoleApp')
         };
 
         $scope.showResponse = function () {
-            return this.responseAvailable;
+            return this.response;
         };
 
         $scope.tryIt = function () {
@@ -19,12 +19,12 @@ angular.module('ramlConsoleApp')
             commons.extend(params, this.url);
             commons.extend(params, this.query[this.operation.method]);
 
+            this.response = null;
             this['$' + this.operation.method](tester, params);
         };
 
         $scope.$get = function (tester, params) {
             var that = this;
-            this.responseAvailable = true;
 
             tester.$get(params, function (data, headers, status, url) {
                 that.response = {
@@ -40,50 +40,82 @@ angular.module('ramlConsoleApp')
                     statusCode: error.status,
                     url: error.config.url
                 };
-
-                console.log(error);
             });
         }
 
-        $scope.transformResponse = function (data) {
-            console.log(data);
-            return { data: data };
+        $scope.$post = function (tester, params) {
+            var that = this;
+
+            tester.$post(params, function (data, headers, status, url) {
+                that.response = {
+                    data: data.data,
+                    headers: data.headers,
+                    statusCode: status,
+                    url: url
+                };
+            }, function (error) {
+                that.response = {
+                    data: error.data.data,
+                    headers: error.data.headers,
+                    statusCode: error.status,
+                    url: error.config.url
+                };
+            });
+        }
+
+        $scope.transformResponse = function (data, headers) {
+            try {
+                data = JSON.parse(data);
+                data = angular.toJson(data, true);
+            }
+            catch (e) {}
+
+            return { data: data, headers: angular.toJson(headers(), true) };
+        };
+
+        $scope.transformRequest = function (data, headers) {
+            return data;
         };
 
         $scope.buildTester = function () {
             var resourceUri = this.baseUri + this.resource.relativeUri.replace(/{/g, ':').replace(/}/g, '');
             this.testerResource = $resource(resourceUri, null, {
-                    'get': {
-                        method:'GET',
-                        isArray: false,
-                        transformResponse: function (data, headers) {
-                            try {
-                                data = JSON.parse(data);
-                                data = angular.toJson(data, true);
-                            }
-                            catch (e) {}
-
-                            return { data: data, headers: angular.toJson(headers(), true) };
-                        },
-                        transformRequest: function (data, headers) {
-                            return data;
-                        }
-                    },
-                    'post': { method:'POST' },
-                    'put': { method:'PUT' },
-                    'delete': { method:'DELETE' }
-                });
+                'get': {
+                    method:'GET',
+                    isArray: false,
+                    transformResponse: this.transformResponse,
+                    transformRequest: this.transformRequest
+                },
+                'post': { method:'POST' },
+                'put': { method:'PUT' },
+                'delete': { method:'DELETE' }
+            });
         };
 
         $scope.init = function () {
-            this.request = {};
-            this.url = {};
-            this.query = { get: {}, put: {}, post: {}, delete: {} };
-            this.requestBody = '';
-            this.responseAvailable = false;
+            if (!this.request) {
+                this.request = {};
+            }
 
+            if (!this.requestBody) {
+                this.requestBody = { put: '', post: '' };
+            }
+
+            if (!this.url) {
+                this.url = {};
+            }
+
+            if (!this.query) {
+                this.query = { get: {}, put: {}, post: {}, delete: {} };
+            }
+
+            this.response = null;
             this.buildTester();
         };
+
+        $scope.$on('event:raml-method-changed', function () {
+            $scope.init();
+        });
 
         $scope.init();
     });
