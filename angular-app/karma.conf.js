c9978d7c9f3908007e8a0d8d150b2499a9b81900
Alberto Pose <albertopose@gmail.com>
2013-09-10 19:36:32 -0300

    Changing karma browser to PhantomJS

--- angular-app/karma.conf.js
+++ angular-app/karma.conf.js
@@ -45,7 +45,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['PhantomJS'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
