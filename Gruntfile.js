29ff2778a67aff8e324a2d67317fc5ee91ee6439
Jeremy Morony <jmorony@carbonfive.com>
2013-09-23 17:50:08 -0700

    Moving around the css to accomodate font awesome.
    Not so awesome though.

--- Gruntfile.js
+++ Gruntfile.js
@@ -305,7 +305,7 @@ module.exports = function (grunt) {
 
                 },
                 files: {
-                    'dist/app.css': 'app/styles/less/main.less'
+                    'dist/styles/app.css': 'app/styles/less/main.less'
                 }
             }
         }
