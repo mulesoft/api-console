327fb6649e230f87729b09452570d7d264121e94
Clark Cutler and Tommy Fisher <dev+clark+tommy@carbonfive.com>
2013-09-30 14:33:00 -0700

    Refactored specs to use new helper methods.

--- spec/integration/controllers/try_it_controller_spec.js
+++ spec/integration/controllers/try_it_controller_spec.js
@@ -1,7 +1,7 @@
 describe("RAML.Controllers.tryIt", function() {
   beforeEach(module('ramlConsoleApp'));
 
-  var scope, el, httpBackend;
+  var scope, $el, httpBackend;
 
   describe('given query parameters', function() {
 
@@ -15,20 +15,20 @@ describe("RAML.Controllers.tryIt", function() {
       '      order:'
     )
 
-    beforeEach(inject(function($httpBackend, $compile, $rootScope) {
+    parseRAML(raml);
+
+    beforeEach(inject(function($httpBackend) {
       httpBackend = $httpBackend;
-      scope = $rootScope.$new();
 
-      var result = parseRAML(raml);
-
-      runs(function() {
-        scope.api = RAML.Inspector.create(result);
+      var parsedApi = this.api;
+      scope = createScope(function(scope) {
+        scope.api = RAML.Inspector.create(parsedApi);
         scope.resource = scope.api.resources[0];
         scope.method = scope.resource.methods[0];
-
-        el = $compile('<try-it></try-it>')(scope);
-        scope.$digest();
+        scope.method.pathBuilder = new RAML.Inspector.PathBuilder.create(scope.resource.pathSegments);
       });
+
+      $el = compileTemplate('<try-it></try-it>', scope);
     }));
 
     afterEach(function() {
@@ -37,10 +37,10 @@ describe("RAML.Controllers.tryIt", function() {
     });
 
     it('executes a request with the provided values', function() {
-      $(el).find('input[name=page]').fillIn('1');
-      $(el).find('input[name=order]').fillIn('newest');
+      $el.find('input[name=page]').fillIn('1');
+      $el.find('input[name=order]').fillIn('newest');
       httpBackend.expectGET('http://www.example.com/resource?order=newest&page=1').respond(200);
-      $(el).find('button[role="try-it"]').click();
+      $el.find('button[role="try-it"]').click();
 
       httpBackend.flush();
     });
--- spec/integration/directives/resource_summary_directive_spec.js
+++ spec/integration/directives/resource_summary_directive_spec.js
@@ -1,25 +1,25 @@
 describe("RAML.Directives.resourceSummary", function() {
   beforeEach(module('ramlConsoleApp'));
 
-  var scope, el;
+  var scope, $el;
 
   describe('given RAML with a resourceType with parameters', function() {
-    beforeEach(inject(function($compile, $rootScope) {
-      scope = $rootScope.$new();
-      scope.resource = {
-        resourceType: {
-          typedcollection: {
-            schema: 'someschema'
+    beforeEach(function() {
+      scope = createScope(function(scope) {
+        scope.resource = {
+          resourceType: {
+            typedcollection: {
+              schema: 'someschema'
+            }
           }
         }
-      }
+      });
 
-      el = $compile('<resource-summary></resource-summary>')(scope);
-      scope.$digest();
-    }));
+     $el = compileTemplate('<resource-summary></resource-summary>', scope);
+   });
 
     it('displays the name of the resourceType', function() {
-      var resourceType = $(el).find("[role=resourceType]").text();
+      var resourceType = $el.find("[role=resourceType]").text();
       expect(resourceType).toEqual('typedcollection');
     });
 
