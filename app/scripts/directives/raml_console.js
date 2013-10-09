ca834a21deef4b3c7111dcc07970c8ac615f654c
Tommy Fisher <tommy@carbonfive.com>
2013-10-09 11:42:52 -0700

    Moved RAML parsing code from directive to controller.

--- app/scripts/directives/raml_console.js
+++ app/scripts/directives/raml_console.js
@@ -1,11 +1,25 @@
 (function() {
   'use strict';
 
-  var Controller = function($scope, $attrs) {
+  var Controller = function($scope, $attrs, ramlParser) {
     $scope.ramlConsole = this;
 
     if ($attrs.hasOwnProperty('withRootDocumentation')) {
       this.withRootDocumentation = true;
+    }
+
+    var success = function(raml) {
+      $scope.api = this.api = RAML.Inspector.create(raml);
+      $scope.$apply();
+    }
+
+    var error = function(error) {
+      $scope.parseError = error;
+      $scope.$apply();
+    }
+
+    if ($scope.src) {
+      ramlParser.loadFile($scope.src).then(success.bind(this), error);
     }
   };
 
@@ -18,35 +32,11 @@
   };
 
   RAML.Directives.ramlConsole = function(ramlParser) {
-    var importAndParseRaml = function(src) {
-      return ramlParser.loadFile(src).then(function (raml) {
-        return raml;
-      });
-    }
 
     var link = function ($scope, $el, $attrs, controller) {
-      var success = function(raml) {
-        $scope.api = RAML.Inspector.create(raml);
-        $scope.$apply();
-      }
-
-      var error = function(error) {
-        $scope.parseError = error;
-        $scope.$apply();
-      }
-
-      // FIXME: move to a controller
-      if ($scope.src) {
-        importAndParseRaml($scope.src).then(success, error);
-      }
-
       // FIXME: move this to the app on module('ramlConsoleApp').run...
       $scope.$on('event:raml-parsed', function(e, raml) {
-        $scope.api = RAML.Inspector.create(raml);
-      });
-
-      $scope.$watch("api", function(api) {
-        controller.api = api;
+        $scope.api = controller.api = RAML.Inspector.create(raml);
       });
     }
 
