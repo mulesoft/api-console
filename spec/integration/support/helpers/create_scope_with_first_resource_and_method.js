79a528202dbe5d85898d765fccb3b4ef4306d2f7
Clark Cutler <clark@carbonfive.com>
2013-10-17 16:33:26 -0700

    Extract helpers from documentation_spec.js

---
+++ spec/integration/support/helpers/create_scope_with_first_resource_and_method.js
@@ -0,0 +1,9 @@
+function createScopeWithFirstResourceAndMethod(parsedApi) {
+  scope = createScope(function(scope) {
+    scope.api = RAML.Inspector.create(parsedApi);
+    scope.resource = scope.api.resources[0];
+    scope.method = scope.resource.methods[0];
+    scope.method.pathBuilder = new RAML.Inspector.PathBuilder.create(scope.resource.pathSegments);
+  });
+  return scope;
+};
