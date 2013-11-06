6ae9d3660f37af4c729eb5c61eaee31fe6789ec7
Jeremy Morony <shamus@carbonfive.com>
2013-11-06 11:44:26 -0800

    Increase runtime of documentation scenario.

--- spec/scenario/console/documentation_scenario.js
+++ spec/scenario/console/documentation_scenario.js
@@ -105,7 +105,7 @@ describe('API Documentation', function() {
       expect(method.getText()).toMatch(new RegExp('<xs:element type="xs:int" name="id"/>'));
       expect(method.getText()).toMatch(new RegExp("<id>1511685</id>"));
     });
-  });
+  }, 10000);
 
   describe("responses tab", function() {
     raml = createRAML(
