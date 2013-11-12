bc073a712da120cb18b2ebb8c042e65c8b0e254a
Tommy Fisher & Jeremy Morony <tommy+shamus@carbonfive.com>
2013-11-11 17:31:03 -0800

    filtering out unsupported Authentication Strategies

--- app/scripts/directives/security_schemes.js
+++ app/scripts/directives/security_schemes.js
@@ -2,10 +2,20 @@
 
 (function() {
   RAML.Directives.securitySchemes = function() {
+
+    var controller = function($scope) {
+      $scope.securitySchemes = this;
+    };
+
+    controller.prototype.supports = function(scheme) {
+      return (scheme.type === 'OAuth 2.0' || scheme.type === 'Basic Authentication');
+    };
+
     return {
       restrict: 'E',
       templateUrl: 'views/security_schemes.tmpl.html',
       replace: true,
+      controller: controller,
       scope: {
         schemes: '=',
         keychain: '='
