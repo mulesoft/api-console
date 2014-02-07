5700676e5ffdf8eab320b21d3c0d91cd980dcdb3
Clark Cutler and Jeremy Morony <dev+clark+jeremy@carbonfive.com>
2014-02-07 14:44:55 -0800

    Refactor method pop-up animation
    - Extract lots of helper functions so it's clearer what's going on
    - Remove dead code, including some unnecessary setTimeouts and
      browser-specific hacks
    - clark dances with joy.  literally.

--- app/scripts/animations/pop-up.js
+++ app/scripts/animations/pop-up.js
@@ -4,110 +4,136 @@
   var animationDuration = 333;
 
   RAML.Animations.popUp = function() {
+    function getElements(placeholder) {
+      return {
+        console: angular.element(document.querySelector('[role="api-console"]')), // FIXME What about multiple consoles on a page; can we just set offsetParent overflow:hidden?
+        offsetParent: angular.element(placeholder[0].offsetParent),
+        placeholder: placeholder,
+        wrapper: placeholder.children().eq(0),
+        resource: placeholder.children().eq(0).children().eq(0),
+        description: angular.element(placeholder[0].querySelector('.description'))
+      }
+    }
+
+    function beginExpansion(placeholder) {
+      placeholder.scope().resourceView.expanded = true;
+      return placeholder[0].getBoundingClientRect();
+    }
+
+    function prepareResourceForAbsolutePositioning(offsetParent, wrapper, resource, bounds) {
+      var scrollOffset = offsetParent[0].scrollTop;
+      var topOffset = offsetParent[0].getBoundingClientRect().top;
+
+      wrapper.css('top', scrollOffset + 'px');
+      resource.css('height', bounds.height + 'px');
+      resource.css('margin-top', bounds.top - 20 - topOffset + 'px'); // 20 padding from wrapper
+    }
+
+    function rememberExpandedResourceBounds(offsetParent, placeholder, resource) {
+      var topOffset = offsetParent[0].getBoundingClientRect().top;
+      var expandedPlaceholderBounds = placeholder[0].getBoundingClientRect();
+      console.log(expandedPlaceholderBounds.height);
+      resource.data('height', expandedPlaceholderBounds.height + 'px');
+      resource.data('margin-top', expandedPlaceholderBounds.top - topOffset + 'px');
+    }
+
+    function prepareDescriptionForAnimation(description) {
+      description.data('height', description[0].getBoundingClientRect().height + 20 + 'px');
+      description.css('height', description.data('height'));
+      setTimeout(function() {
+        description.css('transition', 'height ' + animationDuration/1000 + 's');
+      });
+    }
+
+    function triggerOpenAnimation(resource, description) {
+      resource.css('height', '');
+      resource.css('margin-top', '');
+      description.css('height', '0px');
+    }
+
+    function blockScroll(offsetParent, wrapper, console) {
+      wrapper.css('top', 0);
+      console
+        .data('scrollTop', offsetParent[0].scrollTop)
+        .css('height', '0px')
+        .css('overflow', 'hidden');
+    }
+
+    function afterAnimation(cb) {
+      setTimeout(cb, animationDuration);
+    }
+
+    function restoreScroll(offsetParent, wrapper, console) {
+      var scrollTop = console.data('scrollTop');
+
+      console.css('height', '').css('overflow', '');
+      offsetParent[0].scrollTop = scrollTop;
+      wrapper.css('top', scrollTop + 'px');
+    }
+
+    function triggerCloseAnimation(resource, description) {
+      resource.css('height', resource.data('height'));
+      resource.css('margin-top', parseInt(resource.data('margin-top'), 10) - 20 + 'px');
+      description.css('height', description.data('height'));
+    }
+
     return {
       beforeAddClass: function(element, className, done) {
-        var placeholder = element;
-        var wrapper = angular.element(element.children()[0]);
-        var resource = angular.element(wrapper.children()[0]);
-        var description = angular.element(resource[0].querySelector('.description'));
+        var elements = getElements(element);
 
-        var rect = placeholder[0].getBoundingClientRect();
-        var topOffset = placeholder[0].offsetParent.getBoundingClientRect().top;
-        var scrollOffset = placeholder[0].offsetParent.scrollTop;
-        placeholder.scope().resourceView.expanded = true;
+        var originalPlaceholderBounds = beginExpansion(elements.placeholder);
         setTimeout(function() {
-          var rect2 = placeholder[0].getBoundingClientRect();
-
-          placeholder.css('height', rect.height + 'px');
-          wrapper.css('top', scrollOffset + 'px');
-          resource.css('height', rect.height + 'px');
-          resource.css('margin-top', rect.top - 20 - topOffset + 'px'); // 20 padding from wrapper
-          resource.data('height', rect2.height + 'px');
-          resource.data('margin-top', rect.top - topOffset + 'px');
-
-          description.data('height', description[0].getBoundingClientRect().height + 20 + 'px');
-          description.css('height', description.data('height'));
-          setTimeout(function() {
-            description.css('transition', 'height ' + animationDuration/1000 + 's');
-          });
+          rememberExpandedResourceBounds(elements.offsetParent, elements.placeholder, elements.resource);
+          prepareResourceForAbsolutePositioning(elements.offsetParent, elements.wrapper, elements.resource, originalPlaceholderBounds);
+          elements.placeholder.css('height', originalPlaceholderBounds.height + 'px');
+          prepareDescriptionForAnimation(elements.description);
 
           done();
         });
       },
+
       addClass: function(element, className, done) {
-        var placeholder = element;
-        var wrapper = angular.element(element.children()[0]);
-        var resource = angular.element(element[0].children[0].children[0]);
-        var description = angular.element(resource[0].querySelector('.description'));
+        var elements = getElements(element);
+        elements.resource.scope().$apply('resourceView.expandMethod(methodToAdd)');
 
-        resource.scope().$apply('resourceView.expandMethod(methodToAdd)');
         setTimeout(function() {
-          resource.css('transition', 'height ' + animationDuration/1000 + 's, margin-top ' + animationDuration/1000 + 's');
-          setTimeout(function() {
-            resource.css('height', '');
-            resource.css('margin-top', '');
-            setTimeout(function() {
-              description.css('height', '0px');
-            });
+          triggerOpenAnimation(elements.resource, elements.description)
 
-            setTimeout(function() {
-              placeholder.css('height', resource.data('height'));
-              description.css('visibility', 'hidden');
-              wrapper.css('top', 0);
-              angular.element(document.querySelector('[role="api-console"]'))
-                .data('scrollTop', placeholder[0].offsetParent.scrollTop)
-                .css('height', '0px')
-                .css('overflow', 'hidden');
+          afterAnimation(function() {
+            elements.placeholder.css('height', elements.resource.data('height'));
+            blockScroll(elements.offsetParent, elements.wrapper, elements.console);
 
-              done();
-            }, animationDuration);
+            done();
           });
         });
       },
+
       beforeRemoveClass: function(element, className, done) {
-        var wrapper = angular.element(element.children()[0]);
-        var resource = angular.element(wrapper.children()[0]);
-        var description = angular.element(resource[0].querySelector('.description'));
+        var elements = getElements(element);
 
-        angular.element(document.querySelector('[role="api-console"]'))
-          .css('height', 'auto')
-          .css('overflow', 'visible');
-        var scrollTop = angular.element(document.querySelector('[role="api-console"]')).data('scrollTop');
-        document.querySelector('[role="api-console"]').offsetParent.scrollTop = scrollTop;
-        wrapper.css('top', scrollTop + 'px');
+        restoreScroll(elements.offsetParent, elements.wrapper, elements.console);
+        elements.wrapper.css('background-color', 'transparent');
+        elements.resource.css('height', elements.resource[0].getBoundingClientRect().height + 'px'); // Safari loses the resource's height otherwise
 
-
-        wrapper.css('background-color', 'transparent');
-        description.css('visibility', '');
-        resource.css('height', resource[0].getBoundingClientRect().height + 'px'); // Firefox won't animate from calc(100%-40px) to _px
         setTimeout(function() {
-          resource.css('height', resource.data('height'));
-          resource.css('margin-top', parseInt(resource.data('margin-top'), 10) - 20 + 'px');
-          description.css('height', description.data('height'));
-
-          setTimeout(done, animationDuration);
-        }, 20);
+          triggerCloseAnimation(elements.resource, elements.description);
+          afterAnimation(done);
+        }, 10);
       },
+
       removeClass: function(element, className, done) {
-        var placeholder = element;
-        var wrapper = angular.element(element.children()[0]);
-        var resource = angular.element(wrapper.children()[0]);
-        var description = angular.element(resource[0].querySelector('.description'));
+        var elements = getElements(element);
+        elements.resource.scope().$apply('method = methodToAdd');
 
-        resource.scope().$apply('method = methodToAdd');
+        elements.placeholder.css('height', '');
+        elements.wrapper.css('background-color', '');
+        elements.resource.css({ 'height': '', 'margin-top': '' });
+        elements.description.css({ 'transition': 'height 0s', 'height': '' }); // otherwise Safari incorrectly animates description from 0 to its natural height and freaks out
 
-        placeholder.css('height', '');
-        wrapper.css('background-color', '');
-        resource.css('transition', '');
-        resource.css('height', '');
-        resource.css('margin-top', '');
-        description.css('transition', 'height 0s'); // otherwise Sarfari incorrectly animates description from 0 to its natural height
-        description.css('height', '');
         setTimeout(function() {
-          description.css('transition', '');
+          elements.description.css('transition', '');
+          done();
         });
-
-        done();
       }
     };
   };
