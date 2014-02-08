3c80edf0b28d38013e24d6d9be2801a930481b69
Clark Cutler and Jeremy Morony <dev+clark+jeremy@carbonfive.com>
2014-02-07 17:37:33 -0800

    Add missing semi-colons

--- app/scripts/animations/pop-up.js
+++ app/scripts/animations/pop-up.js
@@ -12,7 +12,7 @@
         wrapper: placeholder.children().eq(0),
         resource: placeholder.children().eq(0).children().eq(0),
         description: angular.element(placeholder[0].querySelector('.description'))
-      }
+      };
     }
 
     function beginExpansion(placeholder) {
@@ -97,7 +97,7 @@
         elements.resource.scope().$apply('resourceView.expandMethod(methodToAdd)');
 
         setTimeout(function() {
-          triggerOpenAnimation(elements.offsetParent, elements.wrapper, elements.resource, elements.description)
+          triggerOpenAnimation(elements.offsetParent, elements.wrapper, elements.resource, elements.description);
 
           afterAnimation(function() {
             elements.placeholder.css('height', elements.resource.data('height'));
