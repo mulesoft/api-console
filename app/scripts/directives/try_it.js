0539dbe1c555bbe4db9772b5546a8783f000e6f4
Jeremy Morony & Ken Shimizu <shamus+ken@carbonfive.com>
2014-02-26 14:10:52 -0800

    Try it re-renders when a new method is selected.
    Before, when a method was selected inside of the resource popover the
    display would not update, showing the try it section for the originally
    selected method.

--- app/scripts/directives/try_it.js
+++ app/scripts/directives/try_it.js
@@ -1,12 +1,18 @@
 (function() {
   'use strict';
 
-  RAML.Directives.tryIt = function() {
+  RAML.Directives.tryIt = function(DataStore) {
     return {
       restrict: 'E',
       templateUrl: 'views/try_it.tmpl.html',
       replace: true,
-      controller: RAML.Controllers.TryIt
+      link: function($scope) {
+        // fix that ensures the try it display is updated
+        // when switching between methods in the resource popover.
+        $scope.$watch('method', function() {
+          new RAML.Controllers.TryIt($scope, DataStore);
+        });
+      }
     };
   };
 })();
