8b01fc8f2cdd629f001150c3fed04fff5c04f06d
Jeremy Morony & Ken Shimizu <shamus+ken@carbonfive.com>
2014-03-14 12:50:27 -0700

    Tightening up animation

--- app/scripts/directives/resource_documentation.js
+++ app/scripts/directives/resource_documentation.js
@@ -8,26 +8,27 @@
       var resourceList = $('#raml-console');
 
       $rootScope.$on('console:expand', function(event, resource, $resourceEl) {
+        var placeholder = $element[0].querySelector('.resource-placeholder');
+        var container = $element[0].querySelector('.resource-container');
+        var rect;
+
         $scope.resource = resource;
         $window.addEventListener('keydown', closeOnEscape);
 
-        var placeholder = $element[0].querySelector('.resource-placeholder');
-        var container = $element[0].querySelector('.resource-container');
-        var rect = $resourceEl[0].getBoundingClientRect();
-
         consoleContainer.css('overflow', 'hidden');
         angular.element($element[0].querySelector('.resource-placeholder')).css('height', resourceList.css('height'));
+        angular.element(container).addClass('grow-expansion-animation');
 
         setTimeout(function() {
+          rect = $resourceEl[0].getBoundingClientRect();
           container.style.top = consoleContainer[0].scrollTop + rect.top - consoleContainer[0].offsetTop + 'px';
           container.style.bottom = consoleContainer[0].scrollTop + rect.bottom + 'px';
           container.style.height = rect.bottom - rect.top + 'px';
-          angular.element(container).addClass('grow-expansion-animation');
 
           setTimeout(function() {
-           angular.element(container).css('height', consoleContainer[0].clientHeight - 10 + 'px');
-           container.style.top = consoleContainer[0].scrollTop + 5 + 'px';
-           angular.element(placeholder).addClass('masked');
+            angular.element(placeholder).addClass('masked');
+            angular.element(container).css('height', consoleContainer[0].clientHeight - 10 + 'px');
+            container.style.top = consoleContainer[0].scrollTop + 5 + 'px';
           });
         });
 
@@ -42,16 +43,16 @@
           container.style.top = consoleContainer[0].scrollTop + rect.top - consoleContainer[0].offsetTop + 'px';
           container.style.bottom = consoleContainer[0].scrollTop + rect.bottom + 'px';
           container.style.height = rect.bottom - rect.top + 'px';
-
           setTimeout(function() {
-            consoleContainer.css('overflow', 'auto');
             angular.element(placeholder).removeClass('masked');
 
             setTimeout(function() {
-              $window.removeEventListener('keydown', closeOnEscape);
               angular.element(container).removeClass('grow-expansion-animation');
+              consoleContainer.css('overflow', 'auto');
+
+              $window.removeEventListener('keydown', closeOnEscape);
               $scope.$apply('resource = undefined');
-            }, 1000);
+            }, 200);
           });
         }
       });
