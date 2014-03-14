92560bef506365d5e1312fb9166c91a36bfba3f6
Jeremy Morony & Ken Shimizu <shamus+ken@carbonfive.com>
2014-03-13 17:10:37 -0700

    Popover animation re-do opening and closing

--- app/scripts/directives/resource_documentation.js
+++ app/scripts/directives/resource_documentation.js
@@ -3,45 +3,57 @@
 
   RAML.Directives.resourceDocumentation = function($window) {
     function Controller($rootScope, $scope, $element) {
+
       var consoleContainer = $('raml-console').parent();
       var resourceList = $('#raml-console');
-
-      function closeOnEscape(e) {
-        if (e.which === 27) {
-          e.preventDefault();
-          $scope.resource = undefined;
-          $window.removeEventListener('keydown', closeOnEscape);
-          $scope.$digest();
-          consoleContainer.css('overflow', 'auto');
-        }
-      }
 
       $rootScope.$on('console:expand', function(event, resource, $resourceEl) {
         $scope.resource = resource;
         $window.addEventListener('keydown', closeOnEscape);
+
+        var placeholder = $element[0].querySelector('.resource-placeholder');
+        var container = $element[0].querySelector('.resource-container');
+        var rect = $resourceEl[0].getBoundingClientRect();
+
         consoleContainer.css('overflow', 'hidden');
         angular.element($element[0].querySelector('.resource-placeholder')).css('height', resourceList.css('height'));
-        angular.element($element[0].querySelector('.resource-container')).css('transition', 'height 1s, top 1s')
-        // console.log(consoleContainer.css('height'));
 
         setTimeout(function() {
-          var placeholder = $element[0].querySelector('.resource-placeholder');
-          var container = $element[0].querySelector('.resource-container');
-          var rect = $resourceEl[0].getBoundingClientRect();
-
           container.style.top = consoleContainer[0].scrollTop + rect.top - consoleContainer[0].offsetTop + 'px';
           container.style.bottom = consoleContainer[0].scrollTop + rect.bottom + 'px';
           container.style.height = rect.bottom - rect.top + 'px';
-          angular.element(placeholder).addClass('test-thing');
+          angular.element(container).addClass('grow-expansion-animation');
 
           setTimeout(function() {
-           // angular.element(container).addClass('grow-container');
-           angular.element(container).css('height', consoleContainer.css('height'));
-           console.log(consoleContainer[0].scrollTop);
-           container.style.top = consoleContainer[0].scrollTop + 'px';
-           // container.style.bottom = '';
+           angular.element(container).css('height', consoleContainer[0].clientHeight - 10 + 'px');
+           container.style.top = consoleContainer[0].scrollTop + 5 + 'px';
+           angular.element(placeholder).addClass('masked');
           });
         });
+
+        function closeOnEscape(e) {
+          if (e.which === 27) {
+            e.preventDefault();
+            closePopover();
+          }
+        }
+
+        function closePopover(e) {
+          container.style.top = consoleContainer[0].scrollTop + rect.top - consoleContainer[0].offsetTop + 'px';
+          container.style.bottom = consoleContainer[0].scrollTop + rect.bottom + 'px';
+          container.style.height = rect.bottom - rect.top + 'px';
+
+          setTimeout(function() {
+            consoleContainer.css('overflow', 'auto');
+            angular.element(placeholder).removeClass('masked');
+
+            setTimeout(function() {
+              $window.removeEventListener('keydown', closeOnEscape);
+              angular.element(container).removeClass('grow-expansion-animation');
+              $scope.$apply('resource = undefined');
+            }, 1000);
+          });
+        }
       });
     }
 
