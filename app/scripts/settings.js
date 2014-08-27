c46d38e5b912733b23953a3145b57aebffc078c2
Blake Embrey <hello@blakeembrey.com>
2014-08-27 19:08:25 -0300

    Tidy settings file

--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -4,10 +4,9 @@
   RAML.Settings = RAML.Settings || {};
 
   var location = window.location;
+  var uri      = location.protocol + '//' + location.host + location.pathname.replace(/\/$/, '');
 
-  var uri = location.protocol + '//' + location.host + location.pathname + 'authentication/oauth2.html';
-  RAML.Settings.oauth2RedirectUri = RAML.Settings.oauth2RedirectUri || uri;
-  RAML.Settings.oauth1RedirectUri = RAML.Settings.oauth1RedirectUri || uri.replace(/oauth2\.html$/, 'oauth1.html');
-
-  // RAML.Settings.proxy = RAML.Settings.proxy || '/proxy/';
+  RAML.Settings.proxy = RAML.Settings.proxy || false;
+  RAML.Settings.oauth2RedirectUri = RAML.Settings.oauth2RedirectUri || uri + '/authentication/oauth2.html';
+  RAML.Settings.oauth1RedirectUri = RAML.Settings.oauth1RedirectUri || uri + '/authentication/oauth2.html';
 })();
