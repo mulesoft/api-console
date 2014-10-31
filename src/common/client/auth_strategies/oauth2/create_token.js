dfd8e12f87bf808e749d3690e1768b238de4f34c
jcenturion <centurion.javier@gmail.com>
2014-10-31 17:56:16 -0300

    Remove unused classes

--- src/common/client/auth_strategies/oauth2/create_token.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  RAML.Client.AuthStrategies.Oauth2.createToken = function(tokenFactory) {
-    return function(token) {
-      return tokenFactory(token);
-    };
-  };
-})();
