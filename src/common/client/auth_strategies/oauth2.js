49006d0f5ef65a76acdae91d45d9d9baac8925cc
jcenturion <centurion.javier@gmail.com>
2015-02-24 14:54:53 -0300

    Make a popup for oAuth dance

--- src/common/client/auth_strategies/oauth2.js
+++ src/common/client/auth_strategies/oauth2.js
@@ -18,6 +18,14 @@
     return scopes;
   }
 
+  function popup(location) {
+    var w    = 640;
+    var h    = 480;
+    var left = (screen.width / 2) - (w / 2);
+    var top  = (screen.height / 2) - (h / 2);
+    return window.open(location, 'Authentication', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+  }
+
   Oauth2.prototype.authenticate = function(options, done) {
     var auth = new ClientOAuth2({
       clientId:         this.credentials.clientId,
@@ -44,7 +52,7 @@
         });
       };
       //// TODO: Find a way to handle 404
-      window.open(auth[grantType].getUri());
+      popup(auth[grantType].getUri());
     }
 
     if (grantType === 'owner') {
