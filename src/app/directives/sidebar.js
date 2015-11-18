692720983314d52dbf0a4b1ec33dcca13a4d5989
Tylor Louis <tylor.louis@mulesoft.com>
2015-11-18 14:51:08 -0800

    Use mediaType for tryIt accept headers. Default to application json

--- src/app/directives/sidebar.js
+++ src/app/directives/sidebar.js
@@ -9,6 +9,7 @@
       controller: ['$scope', '$timeout', function ($scope, $timeout) {
         var defaultSchemaKey = Object.keys($scope.securitySchemes).sort()[0];
         var defaultSchema    = $scope.securitySchemes[defaultSchemaKey];
+        var defaultAccept    = 'application/json';
 
         $scope.markedOptions     = RAML.Settings.marked;
         $scope.currentSchemeType = defaultSchema.type;
@@ -410,12 +411,12 @@
               $scope.response = {};
               return;
             }
-
             var request = RAML.Client.Request.create(url, $scope.methodInfo.method);
 
             $scope.parameters = getParameters(context, 'queryParameters');
 
             request.queryParams($scope.parameters);
+            request.header('Accept', $scope.raml.mediaType || defaultAccept);
             request.headers(getParameters(context, 'headers'));
 
             if (context.bodyContent) {
@@ -445,7 +446,6 @@
                 });
                 return;
               }
-
               authStrategy = RAML.Client.AuthStrategies.forScheme(scheme, $scope.credentials);
               authStrategy.authenticate().then(function(token) {
                 token.sign(request);
