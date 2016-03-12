30020b8825349fe85ce2a8bb5139fef436bd6d73
Pavel Strashkin <pavel.strashkin@mulesoft.com>
2016-03-11 18:09:44 -0800

    Split into more directives based on responsibilities

---
+++ src/app/directives/raml-console-loader.js
@@ -0,0 +1,58 @@
+(function () {
+  'use strict';
+
+  angular.module('RAML.Directives')
+    .directive('ramlConsoleLoader', function ramlConsoleLoader() {
+      return {
+        restrict:    'E',
+        templateUrl: 'directives/raml-console-loader.tpl.html',
+        replace:     true,
+        controller:  'RamlConsoleLoaderController',
+        scope:       {
+          src:     '@',
+          options: '='
+        }
+      };
+    })
+    .controller('RamlConsoleLoaderController', function RamlConsoleLoaderController(
+      $scope,
+      ramlParser
+    ) {
+      $scope.vm = {
+        error:   void(0),
+        loaded:  false,
+        options: $scope.options,
+        raml:    void(0),
+        src:     $scope.src
+      };
+
+      // ---
+
+      (function activate() {
+        loadFromUrl($scope.vm.src);
+      })();
+
+      // ---
+
+      function loadFromUrl(url) {
+        $scope.vm.raml   = void(0);
+        $scope.vm.loaded = false;
+        $scope.vm.error  = void(0);
+
+        return ramlParser.loadFile(url)
+          .then(function (raml) {
+            $scope.vm.raml = raml;
+          })
+          .catch(function (error) {
+            $scope.vm.error = angular.extend(error, {
+              buffer: (error.context_mark || error.problem_mark).buffer
+            });
+          })
+          .finally(function () {
+            $scope.vm.loaded = true;
+          })
+        ;
+      }
+    })
+  ;
+})();
