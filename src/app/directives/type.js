a60f312f1a20a748292fa5f8578f9d9b4c6d7e94
Juan Coen <juan.coen@mulesoft.com>
2016-04-06 11:28:54 -0300

    Fixed bug in type popovers
    - Convert types to Array to maintain order correctly
    - Use events to manage popover toggling

--- src/app/directives/type.js
+++ src/app/directives/type.js
@@ -2,6 +2,7 @@
   'use strict';
 
   RAML.Directives.type = function() {
+    var TOGGLE_POPOVER = 'popover:toggle';
     return {
       restrict: 'E',
       templateUrl: 'directives/type.tpl.html',
@@ -9,19 +10,33 @@
         typeName: '=',
         hideTypeLinks: '='
       },
-      controller: function ($scope, $rootScope) {
+      controller: function ($scope, $rootScope, $timeout) {
         $scope.typeInfo = RAML.Inspector.Types.getTypeInfo($scope.typeName);
 
-        $scope.selectType = function (type) {
+        $scope.closePopover = function () {
+          $scope.selectedType = null;
+        }
+
+        $rootScope.$on(TOGGLE_POPOVER, function () {
+          $scope.closePopover();
+        });
+
+        $scope.selectType = function ($event, type) {
           jQuery(document).one('click', function () {
-            $scope.selectedType = null;
+            $timeout(function () {
+              $rootScope.$broadcast(TOGGLE_POPOVER);
+            });
           });
+
+          $rootScope.$broadcast(TOGGLE_POPOVER);
 
           $scope.selectedType = RAML.Inspector.Types.mergeType({
               displayName: type,
               type: [type]
             },
             $rootScope.types);
+
+          $event.stopPropagation();
         };
       }
     };
