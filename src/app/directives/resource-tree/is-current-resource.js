6d0bc73c0f0d75bc93a2d840044740e0dd7fcfea
Carolina Wright <carolina.wright@Carolinas-MacBook-Pro.local>
2017-05-29 12:02:50 -0300

    jshint fixes

--- src/app/directives/resource-tree/is-current-resource.js
+++ src/app/directives/resource-tree/is-current-resource.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('RAML.Directives')
-    .factory('isCurrentResource', ['$rootScope', 'resourceId',function resourceId($rootScope, resourceId) {
+    .factory('isCurrentResource', ['$rootScope', 'resourceId',function resource($rootScope, resourceId) {
       return function($scope, resource) {
         return $scope.currentId && $rootScope.currentId === resourceId(resource);
       };
