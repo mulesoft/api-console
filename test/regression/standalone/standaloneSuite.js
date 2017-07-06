38f905d410900e733396c0fe6ebfb5778f006807
Pawel Psztyc <jarrodek@gmail.com>
2017-07-06 17:28:43 +0100

    Removed v3 source files.

--- test/regression/standalone/standaloneSuite.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-describe('Standalone', function () {
-  xdescribe('Initializer', require('./initializerSpec'));
-  describe('Directive', require('./directiveSpec'));
-  xdescribe('URI Parameter', require('./uriParamSpec'));
-});
