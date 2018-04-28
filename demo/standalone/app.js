00327aa7c840984ccb69349a130c4b1551612258
Pawel Psztyc <jarrodek@gmail.com>
2018-04-28 16:17:07 -0700

    Update: Updated standalone app demo

--- demo/standalone/app.js
+++ demo/standalone/app.js
@@ -32,49 +32,93 @@
    */
   apiconsole.app.init = function() {
     apiconsole.app.setInitialRouteData();
-    apiconsole.app.addParserListeners();
     apiconsole.app.observeRouteEvents();
     apiconsole.app.observeConsoleControls();
+    apiconsole.app.observeAmfParser();
+    let dataElement = document.querySelector('parser-element');
+    dataElement.removeAttribute('hidden');
   };
-
+  /**
+   * Reads inital route data from the `app-location` component.
+   * If the route has any data the it is stored as a apiconsole.app.__initial*
+   * propertues which are eventually restored after AMF model is set.
+   *
+   * Note that setting amfModel automatically resets navigation to
+   * `/summary`
+   */
   apiconsole.app.setInitialRouteData = function() {
     // sets the initial path for routing from external source.
-    // The API console sets default path to `summary` after RAML change.
+    // The API console sets default path to `summary` after AMF data change.
     const location = document.querySelector('app-location');
-    const locationPath = location.path;
+    let locationPath = location.path;
     if (!locationPath) {
       return;
     }
-    let parsedPath = locationPath.replace(/\-/g, '.');
-    if (parsedPath[0] === '/') {
-      parsedPath = parsedPath.substr(1);
+    if (locationPath[0] === '/') {
+      locationPath = locationPath.substr(1);
     }
-    let _route = parsedPath.split('/');
+    let _route = locationPath.split('/');
     let page = _route[0];
-    let path = _route[1];
-
-    apiconsole.app.__initialPage = page;
-    apiconsole.app.__initialPath = path;
+    let type = _route[1];
+    let selected = _route[2];
+    if (page) {
+      apiconsole.app.__initialPage = page;
+    }
+    if (type) {
+      apiconsole.app.__initialType = type;
+    }
+    if (selected) {
+      apiconsole.app.__initialSelected = decodeURIComponent(selected);
+    }
   };
   /**
-   * Adds event listeres to elements that are related to RAML dataq parsing.
+   * This is custom thing for this demo page and has nothing to do with
+   * API console
    */
-  apiconsole.app.addParserListeners = function() {
-    document.querySelector('raml-docs-parser')
-    .addEventListener('raml-ready', function(e) {
-      let apiConsole = document.querySelector('api-console');
-      apiConsole.raml = e.detail.raml;
+  apiconsole.app.observeAmfParser = function() {
+    const dataElement = document.querySelector('parser-element');
+    dataElement.addEventListener('amf-changed', apiconsole.app.loadAmf);
+  };
+  /**
+   * Handler for the `amf-changed` event dispatched by `parser-element`
+   * (see ../parser-element/parser-element.html).
+   * Sets the AMF model on `raml-aware` element and renders the console.
+   *
+   * You can set the model of console's `amfModel` property instead of using
+   * `raml-aware`. It just illustrates that you can do this.
+   *
+   * Second part of this function is related to resetting layout and setting
+   * routes.
+   * When the element is initialized and it's invisible (as in this example)
+   * the layout elements that positions headers and toolbars can't properly
+   * calculate the position of fixed elements. As a result console's toolbars
+   * renders incorrectly. In this case call `resetLayout()` function. This
+   * will inform layout elements to reclaculare styles.
+   * You should do this each time the element is re-rendered s browser window
+   * could change size.
+   *
+   * @param {CustomEvent} e
+   */
+  apiconsole.app.loadAmf = function(e) {
+    const model = e.detail.value;
+    let aware = document.querySelector('raml-aware');
+    aware.raml = model;
+    e.target.setAttribute('hidden', true);
+    let apic = document.querySelector('api-console');
+    apic.removeAttribute('hidden');
 
-      if (apiconsole.app.__initialPage &&
-        apiconsole.app.__initialPage !== apiConsole.page) {
-        apiconsole.app.pageChanged(apiconsole.app.__initialPage);
-        apiconsole.app.__initialPage = undefined;
+    requestAnimationFrame(() => {
+      apic.resetLayout();
+      if (apiconsole.app.__initialType && apiconsole.app.__initialSelected) {
+        apiconsole.app.selectionChanged(
+          apiconsole.app.__initialSelected,
+          apiconsole.app.__initialType,
+          apiconsole.app.__initialPage
+        );
       }
-      if (apiconsole.app.__initialPath &&
-        apiconsole.app.__initialPath !== apiConsole.path) {
-        apiconsole.app.pathChanged(apiconsole.app.__initialPath);
-        apiconsole.app.__initialPath = undefined;
-      }
+      apiconsole.app.__initialPage = undefined;
+      apiconsole.app.__initialSelected = undefined;
+      apiconsole.app.__initialType = undefined;
     });
   };
   /**
@@ -82,31 +126,35 @@
    * app-location, app-route and api-console.
    */
   apiconsole.app.observeRouteEvents = function() {
-    let apiConsole = document.querySelector('api-console');
+    let apic = document.querySelector('api-console');
     let location = document.querySelector('app-location');
 
-    apiConsole.addEventListener('path-changed', apiconsole.app._pathChanged);
-    apiConsole.addEventListener('page-changed', apiconsole.app._pageChanged);
+    apic.addEventListener('api-navigation-selection-changed', apiconsole.app._selectionChanged);
+    apic.addEventListener('page-changed', apiconsole.app._pageChanged);
     location.addEventListener('route-changed', apiconsole.app._routeChanged);
   };
-  // Event handler for the path change.
-  apiconsole.app._pathChanged = function(e) {
-    apiconsole.app.pathChanged(e.detail.value);
-  };
-  // Called when path changed from the api-console.
-  apiconsole.app.pathChanged = function(path) {
-    if (!path) {
+  // Event handler for the selection change.
+  apiconsole.app._selectionChanged = function(e) {
+    if (e.detail.passive === true) {
       return;
     }
+    apiconsole.app.selectionChanged(e.detail.selected, e.detail.type, e.target.page);
+  };
+  // Called when path changed from the api-console.
+  apiconsole.app.selectionChanged = function(selected, type, page) {
+    if (!selected || !type) {
+      return;
+    }
+    page = page || 'docs';
     let location = document.querySelector('app-location');
-    let parsedPath = path.replace(/\./g, '-');
-    let newPath = '/docs/' + parsedPath;
+    let newPath = [page, type, encodeURIComponent(selected)].join('/');
     if (newPath !== location.path) {
       location.set('path', newPath);
     }
   };
   // Event handler for the page change.
   apiconsole.app._pageChanged = function(e) {
+    apiconsole.app.selectionChanged(e.target.selectedShape, e.target.selectedShapeType, e.detail.value);
     apiconsole.app.pageChanged(e.detail.value);
   };
   // Called when page change.
@@ -127,19 +175,25 @@
       document.querySelector('app-location').set('path', '/docs');
       return;
     }
-    let parsedPath = locationPath.replace(/\-/g, '.');
-    if (parsedPath[0] === '/') {
-      parsedPath = parsedPath.substr(1);
+    if (locationPath[0] === '/') {
+      locationPath = locationPath.substr(1);
     }
-    let _route = parsedPath.split('/');
+    let _route = locationPath.split('/');
     let page = _route[0];
-    let path = _route[1];
-    let apiConsole = document.querySelector('api-console');
-    if (apiConsole.page !== page) {
-      apiConsole.page = page;
+    let type = _route[1];
+    let selected = _route[2];
+    if (selected) {
+      selected = decodeURIComponent(selected);
     }
-    if (apiConsole.path !== path) {
-      apiConsole.path = path;
+    let apic = document.querySelector('api-console');
+    if (apic.page !== page) {
+      apic.page = page;
+    }
+    if (apic.selectedShapeType !== type) {
+      apic.selectedShapeType = type;
+    }
+    if (apic.selectedShape !== selected) {
+      apic.selectedShape = selected;
     }
   };
   /**
@@ -165,29 +219,33 @@
 
   apiconsole.app.observeConsoleControls = function() {
     let toggles = document.querySelectorAll(
-      '.console-controls paper-toggle-button');
+      '[data-action]');
     for (let i = 0, len = toggles.length; i < len; i++) {
       toggles[i].addEventListener('checked-changed',
+        apiconsole.app.consoleControlsChanged);
+    }
+
+    let clickAction = document.querySelectorAll('[data-click-action]');
+    for (let i = 0, len = clickAction.length; i < len; i++) {
+      clickAction[i].addEventListener('click',
         apiconsole.app.consoleControlsChanged);
     }
   };
 
   apiconsole.app.consoleControlsChanged = function(e) {
-    let action = e.target.dataset.action;
+    let action = e.target.dataset.action || e.target.dataset.clickAction;
     switch (action) {
-      case 'parser':
-        let parser = document.querySelector('raml-docs-parser');
-        if (e.detail.value) {
-          parser.removeAttribute('hidden');
-        } else {
-          parser.setAttribute('hidden', true);
-        }
-        break;
       case 'noTryIt':
       case 'narrow':
       case 'noAttribution':
         let apiConsole = document.querySelector('api-console');
         apiConsole[action] = e.detail.value;
+        break;
+      case 'goBack':
+        let apic = document.querySelector('api-console');
+        apic.setAttribute('hidden', true);
+        let dataElement = document.querySelector('parser-element');
+        dataElement.removeAttribute('hidden');
         break;
     }
   };
@@ -196,5 +254,9 @@
   apiconsole.app.notifyInitError = function(message) {
     window.alert('Cannot initialize API console. ' + message);
   };
-  apiconsole.app.init();
+  if (window.WebComponents && window.WebComponents.ready) {
+    apiconsole.app.init();
+  } else {
+    window.addEventListener('WebComponentsReady', apiconsole.app.init);
+  }
 })();
