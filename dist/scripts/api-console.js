3da553de61b81e9ae1fd737b4e0fa303c06a8f39
marcoskhabie <marcoskhabie@hotmail.com>
2019-06-24 13:28:21 -0300

    bugfix IE not working console CONSOLE-413

--- dist/scripts/api-console.js
+++ dist/scripts/api-console.js
@@ -5333,6 +5333,20 @@ RAML.Inspector = (function() {
   }
 })();
 
+(function () {
+  /* jshint -W034 */
+  'use strict';
+
+  if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+      value: function(search, pos) {
+        pos = !pos || pos < 0 ? 0 : +pos;
+        return this.substring(pos, pos + search.length) === search;
+      }
+    });
+  }
+})();
+
 (function() {
   'use strict';
 
