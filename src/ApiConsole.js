cc1bc3c4652b06e51c5df69b1de04a0b009f567e
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-04-21 10:27:42 -0300

    feat(api-documentation): hide server selector if narrow mode

--- src/ApiConsole.js
+++ src/ApiConsole.js
@@ -315,6 +315,7 @@ export class ApiConsole extends AmfHelperMixin(LitElement) {
       @api-navigation-selection-changed="${this._apiNavigationOcurred}"
       .selectedServerValue="${selectedServerValue}"
       .selectedServerType="${selectedServerType}"
+      .noServerSelector="${narrow}"
     >
       <slot name="custom-base-uri" slot="custom-base-uri"></slot>
     </api-documentation>`;
