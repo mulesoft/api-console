a686d4d78d17bfb454a6c726855831b1726a7b50
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-05-27 10:54:59 -0300

    Revert "fix(server): set type before value"
    This reverts commit 9140820274c7fefd4de4cb0c7795cde79d7942e0.

--- src/ApiConsole.js
+++ src/ApiConsole.js
@@ -650,8 +650,8 @@ export class ApiConsole extends AmfHelperMixin(LitElement) {
    */
   _handleServerChange(e) {
     const { value, type } = e.detail;
-    this.serverType = type;
     this.serverValue = value;
+    this.serverType = type;
   }
 
   render() {
