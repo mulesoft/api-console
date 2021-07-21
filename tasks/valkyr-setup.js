48c237fbeecd5e6743aef5c4e936271203978a52
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2021-07-21 18:23:09 -0300

    build: remove build-arg

--- tasks/valkyr-setup.js
+++ tasks/valkyr-setup.js
@@ -2,4 +2,5 @@ if (process.env.NODE_ENV === "production") {
   process.env.PLAYWRIGHT_BROWSERS_PATH = "./.pw-browsers";
 } else {
     console.log('NODE_ENV: ', process.env.NODE_ENV);
+    console.log('env variables: ', JSON.stringify(process.env, null , 2));
 }
