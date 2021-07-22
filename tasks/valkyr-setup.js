dd03fd0ad8e1a368b149546174eb026743b6f05f
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2021-07-22 11:08:14 -0300

    build: try checking permissions on cache folder

--- tasks/valkyr-setup.js
+++ tasks/valkyr-setup.js
@@ -1,6 +1,20 @@
-if (process.env.NODE_ENV === "production") {
+const fs = require("fs");
+
+function hasAccessToCache() {
+  try {
+    fs.accessSync("\\AppData\\Local\\ms-playwright");
+    console.log(fs.readdirSync("\\AppData\\Local\\ms-playwright"))
+  } catch (e) {
+    console.log("Does not have access", e);
+    return false;
+  }
+  console.log("Does have access");
+  return true;  
+}
+
+if (process.env.NODE_ENV === "production" || !hasAccessToCache()) {
   process.env.PLAYWRIGHT_BROWSERS_PATH = "./.pw-browsers";
 } else {
-    console.log('NODE_ENV: ', process.env.NODE_ENV);
-    console.log('env variables: ', JSON.stringify(process.env, null , 2));
+  console.log("NODE_ENV: ", process.env.NODE_ENV);
+  // console.log("env variables: ", JSON.stringify(process.env, null, 2));
 }
