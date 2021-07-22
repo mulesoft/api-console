e096d7003e72935740ac00841d1856476a1c3030
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2021-07-22 12:10:02 -0300

    build: add shelljs, install playwright with local browsers if needed

--- tasks/valkyr-setup.js
+++ tasks/valkyr-setup.js
@@ -1,28 +1,27 @@
 const fs = require("fs");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const shell = require("shelljs");
 
 function hasAccessToCache() {
   const cachePathMap = {
-    darwin: '/Library/Caches',
-    linux: '/AppData/Local'
-  }
+    darwin: "/Library/Caches",
+    linux: "/AppData/Local",
+  };
   const { platform } = process;
-  console.log('Platform:', platform)
+  console.log("Platform:", platform);
   try {
     const cachePath = cachePathMap[platform];
     fs.accessSync(cachePath);
-    console.log(fs.readdirSync(cachePath))
+    console.log(fs.readdirSync(cachePath));
   } catch (e) {
     console.log("Does not have access", e);
     return false;
   }
   console.log("Does have access");
-  return true;  
+  return true;
 }
 
 if (process.env.NODE_ENV === "production" || !hasAccessToCache()) {
-  process.env.PLAYWRIGHT_BROWSERS_PATH = "./.pw-browsers";
-} else {
-  console.log("NODE_ENV: ", process.env.NODE_ENV);
-  // console.log("env variables: ", JSON.stringify(process.env, null, 2));
+  console.log("Installing playwright with local browsers path.");
+  shell.exec("PLAYWRIGHT_BROWSERS_PATH=0 npm i -D playwright");
 }
- 
