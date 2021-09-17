219f83053f1a45da788bcf5b293f66b3272c8e1e
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2021-09-17 14:54:08 -0300

    [APIC-715] 6.0.6 (#716)
    * 6.0.6
    
    * build: add CODEOWNERS
    
    * build: add deployment.yml
    
    * build: remove, karma and travis
    
    * build: migrate to web-test-runner
    
    * test: add timeout for a unit test

---
+++ web-test-runner.config.mjs
@@ -0,0 +1,30 @@
+export default {
+    files: 'test/**/*.test.js',
+    nodeResolve: true,
+    middleware: [
+      function rewriteBase(context, next) {
+        if (context.url.indexOf('/base') === 0) {
+          context.url = context.url.replace('/base', '');
+        }
+        return next();
+      }
+    ],
+    coverageConfig: {
+      include: ['src/**.js'],
+    },
+    testFramework: {
+      config: {
+        timeout: 600000,
+      },
+    },
+    browserStartTimeout: 20000,
+    testsStartTimeout: 20000,
+    testsFinishTimeout: 600000,
+    testRunnerHtml: (testFramework) =>
+    `<html>
+    <body>
+      <script src="./demo/vendor.js"></script>
+      <script type="module" src="${testFramework}"></script>
+    </body>
+    </html>`
+  };
