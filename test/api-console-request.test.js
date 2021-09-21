ce15b4ebbb9a4960d53b9d2f873463736a747628
Carolina Wright <carolina.wright@mulesoft.com>
2021-09-21 15:39:53 -0300

    test: authorization in request panel

--- test/api-console-request.test.js
+++ test/api-console-request.test.js
@@ -5,7 +5,7 @@ import '../api-console.js';
 import {
   documentationTryItButton,
   navigationSelectEndpointMethod,
-  requestBodySection,
+  requestBodySection, requestCredentialsSection,
   requestHeadersSection,
   requestQueryParamSection, requestSendButton,
   requestUrlSection
@@ -132,6 +132,175 @@ describe('API Console request', () => {
           const body = requestBodySection(element);
           assert.exists(body.shadowRoot.querySelector('raw-payload-editor'));
         });
+      });
+
+      describe('Authorization', () => {
+        const assertDropdownMenu = (form, name, menuLabel, value) => {
+          const menu = form.querySelector(`anypoint-dropdown-menu[name="${name}"]`);
+          assert.exists(menu);
+          assert.exists(menu.shadowRoot.querySelector('.label').innerText, menuLabel);
+          assert.exists(menu.shadowRoot.querySelector('.input-wrapper').innerText, value);
+        }
+
+        const assertMaskedInput = (form, name, inputLabel) => {
+          const input = form.querySelector(`anypoint-masked-input[name="${name}"]`);
+          assert.exists(input);
+          assert.exists(input.querySelector('label').innerText, inputLabel);
+        }
+
+        const assertInput = (form, name, inputLabel) => {
+          const input = form.querySelector(`anypoint-input[name="${name}"]`);
+          assert.exists(input);
+          assert.exists(input.querySelector('label').innerText, inputLabel);
+        }
+
+        describe('x-other', () => {
+          let credentialsSection
+
+          beforeEach(async () => {
+            await navigationSelectEndpointMethod(element, '/test-custom-scheme', 'get');
+            await aTimeout(50)
+            documentationTryItButton(element).click()
+            await aTimeout(50)
+            credentialsSection = requestCredentialsSection(element);
+          });
+
+          it(`should render credentials section`, async () => {
+            assert.exists(credentialsSection);
+          });
+
+          it(`should render auth label`, async () => {
+            assert.equal(credentialsSection.shadowRoot.querySelector('.auth-selector-label').innerText, 'x-custom');
+          });
+
+          it(`should render authorization method`, async () => {
+            const authorizationMethod = credentialsSection.shadowRoot.querySelector('api-authorization-method');
+            assert.equal(authorizationMethod.getAttribute('type'), 'custom');
+
+            const authorizationMethodTitle = authorizationMethod.shadowRoot.querySelector('.subtitle');
+            assert.equal(authorizationMethodTitle.querySelector('span').innerText, 'Scheme: customScheme');
+            assert.exists(authorizationMethodTitle.querySelector('.hint-icon'));
+          });
+
+          it(`should render scheme fields`, async () => {
+            const authorizationMethod = credentialsSection.shadowRoot.querySelector('api-authorization-method');
+            const authorizationMethodForm = authorizationMethod.shadowRoot.querySelector('form');
+            assert.equal(authorizationMethodForm.querySelector('.section-title').innerText, 'Headers');
+
+            const fields = authorizationMethodForm.querySelectorAll('.field-value');
+            assert.lengthOf(fields, 1);
+            const formItem = fields[0].querySelector('api-form-item');
+            const input = formItem.shadowRoot.querySelector('anypoint-input');
+            assert.equal(input.querySelector('label').innerText, 'SpecialToken*');
+            assert.exists(fields[0].querySelector('.hint-icon'));
+          });
+
+          describe('Request with credentials', () => {
+            beforeEach(async () => {
+              spy = sinon.spy();
+              document.body.addEventListener('api-request', spy);
+            });
+
+            it(`should add all credential headers to request`, async () => {
+              requestSendButton(element).click();
+              await nextFrame();
+
+              assert.isTrue(spy.called);
+              assert.equal(spy.getCall(0).args[0].detail.headers, 'SpecialToken: special-token');
+            });
+          });
+        })
+
+        describe('Oauth 1.0', () => {
+          let credentialsSection
+
+          beforeEach(async () => {
+            await navigationSelectEndpointMethod(element, '/test-oauth10-scheme', 'get');
+            await aTimeout(50)
+            documentationTryItButton(element).click()
+            await aTimeout(50)
+            credentialsSection = requestCredentialsSection(element);
+          });
+
+          it(`should render credentials section`, async () => {
+            assert.exists(credentialsSection);
+          });
+
+          it(`should render auth label`, async () => {
+            assert.equal(credentialsSection.shadowRoot.querySelector('.auth-selector-label').innerText, 'OAuth 1.0');
+          });
+
+          it(`should render authorization method`, async () => {
+            const authorizationMethod = credentialsSection.shadowRoot.querySelector('api-authorization-method');
+            assert.equal(authorizationMethod.getAttribute('type'), 'oauth 1');
+          });
+
+          it(`should render scheme fields`, async () => {
+            const authorizationMethod = credentialsSection.shadowRoot.querySelector('api-authorization-method');
+            const authorizationMethodForm = authorizationMethod.shadowRoot.querySelector('form');
+
+            assertDropdownMenu(authorizationMethodForm, 'authTokenMethod', 'Authorization token method', 'POST')
+            assertDropdownMenu(authorizationMethodForm, 'authParamsLocation', 'Oauth parameters location', 'Authorization header')
+            assertDropdownMenu(authorizationMethodForm, 'signatureMethod', 'Signature method', 'HMAC-SHA1')
+
+            assertMaskedInput(authorizationMethodForm, 'consumerKey', 'Consumer key')
+            assertMaskedInput(authorizationMethodForm, 'consumerSecret', 'Consumer secret')
+            assertMaskedInput(authorizationMethodForm, 'token', 'Token')
+            assertMaskedInput(authorizationMethodForm, 'tokenSecret', 'Token secret')
+            assertMaskedInput(authorizationMethodForm, 'realm', 'Realm')
+
+            assertInput(authorizationMethodForm, 'requestTokenUri', 'Request token URI')
+            assertInput(authorizationMethodForm, 'accessTokenUri', 'Token Authorization URI')
+            assertInput(authorizationMethodForm, 'authorizationUri', 'User authorization dialog URI')
+            assertInput(authorizationMethodForm, 'redirectUri', 'Redirect URI')
+            assertInput(authorizationMethodForm, 'timestamp', 'Timestamp')
+            assertInput(authorizationMethodForm, 'nonce', 'Nonce')
+
+            assert.exists(authorizationMethod.shadowRoot.querySelector('.auth-button'));
+          });
+        })
+
+        describe('Oauth 2.0', () => {
+          let credentialsSection
+
+          beforeEach(async () => {
+            await navigationSelectEndpointMethod(element, '/test-oauth20-scheme', 'get');
+            await aTimeout(50)
+            documentationTryItButton(element).click()
+            await aTimeout(50)
+            credentialsSection = requestCredentialsSection(element);
+          });
+
+          it(`should render credentials section`, async () => {
+            assert.exists(credentialsSection);
+          });
+
+          it(`should render auth label`, async () => {
+            assert.equal(credentialsSection.shadowRoot.querySelector('.auth-selector-label').innerText, 'OAuth 2.0');
+          });
+
+          it(`should render authorization method`, async () => {
+            const authorizationMethod = credentialsSection.shadowRoot.querySelector('api-authorization-method');
+            assert.equal(authorizationMethod.getAttribute('type'), 'oauth 2');
+          });
+
+          it(`should render scheme fields`, async () => {
+            const authorizationMethod = credentialsSection.shadowRoot.querySelector('api-authorization-method');
+            const authorizationMethodForm = authorizationMethod.shadowRoot.querySelector('form');
+
+            assertDropdownMenu(authorizationMethodForm, 'grantType', 'Response type', 'Access token')
+            assertMaskedInput(authorizationMethodForm, 'clientId', 'Client id')
+            assertInput(authorizationMethodForm, 'authorizationUri', 'Authorization URI')
+
+            const scopes = authorizationMethod.shadowRoot.querySelector('oauth2-scope-selector');
+            assert.exists(scopes);
+            assert.equal(scopes.shadowRoot.querySelector('.form-label').innerText, 'Scopes');
+            assert.exists(scopes.shadowRoot.querySelector('.scope-input'));
+
+            assert.exists(authorizationMethod.shadowRoot.querySelector('.redirect-section span').innerText, 'https://auth.advancedrestclient.com/oauth-popup.html');
+            assert.exists(authorizationMethod.shadowRoot.querySelector('.auth-button'));
+          });
+        })
       });
     });
   });
