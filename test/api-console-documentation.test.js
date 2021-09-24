3f616c8ba1a32a95ad528fdb8a5a84c241aa285f
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2021-09-24 17:30:03 -0300

    API Console v6.6.4 (#720)
    * chore(deps): regenerate package-lock.json
    
    * feat: upgrade api-request
    
    * 6.6.4
    
    * test: added some timeouts to tests
    
    * test: added some timeouts to tests
    
    * test: added some timeouts to tests
    
    * test: added some timeouts to tests
    
    * test: added some timeouts to tests

--- test/api-console-documentation.test.js
+++ test/api-console-documentation.test.js
@@ -46,6 +46,7 @@ describe('API Console documentation', () => {
         });
 
         it(`should render summary documentation panel`, async () => {
+          await aTimeout(100);
           assert.ok(documentationPanel(element));
           assert.ok(documentationSummary(element));
         });
@@ -53,14 +54,18 @@ describe('API Console documentation', () => {
         it(`should render basic summary documentation`, async () => {
           const summaryShadowRoot = documentationSummary(element).shadowRoot;
           const title = summaryShadowRoot.querySelector('.api-title').innerText;
+          await aTimeout(200);
           assert.equal(title.trim(), 'API title: Google Drive')
           const version = summaryShadowRoot.querySelector('.inline-description.version').innerText;
+          await aTimeout(200);
           assert.equal(version.trim(), 'Version: v2')
 
           const url = summaryShadowRoot.querySelector('api-url').shadowRoot;
           const baseUri = url.querySelector('.url-area > .url-value').innerText;
+          await aTimeout(200);
           assert.equal(baseUri.trim(), 'https://www.googleapis.com/drive/{version}')
           const description = summaryShadowRoot.querySelector('.marked-description').innerText;
+          await aTimeout(200);
           assert.equal(description.trim(), 'Google Drive API')
         });
 
@@ -68,12 +73,15 @@ describe('API Console documentation', () => {
           const documentation = documentationSummary(element);
           const summaryShadowRoot = documentation.shadowRoot;
           const endpointsSection = summaryShadowRoot.querySelector('.toc');
+          await aTimeout(200);
           assert.ok(endpointsSection)
 
           const endpoints = summaryShadowRoot.querySelectorAll('.endpoint-item');
+          await aTimeout(200);
           assert.lengthOf(endpoints,32);
 
           const files = endpoints[0];
+          await aTimeout(200);
           assert.equal(files.querySelector('.endpoint-path').innerText,'Files');
           assert.equal(files.querySelector('.endpoint-path-name').innerText,'/files');
           assert.ok(files.querySelector('.endpoint-header'));
