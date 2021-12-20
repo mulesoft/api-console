afc7689f1dfc02c9ba418495913b379caf247bcd
Carolina Wright <carolina.wright@mulesoft.com>
2021-12-20 11:06:49 -0300

    test: waitUntil added to fix tests

--- test/api-console-request.test.js
+++ test/api-console-request.test.js
@@ -476,7 +476,8 @@ describe('API Console request', () => {
             assert.equal(credentialsSection.shadowRoot.querySelector('.auth-selector-label').innerText, 'Pass Through');
           });
 
-          it('should render authorization method', () => {
+          it('should render authorization method', async () => {
+            await waitUntil(() => Boolean(credentialsSection.shadowRoot.querySelector('api-authorization-method')));
             const authorizationMethod = credentialsSection.shadowRoot.querySelector('api-authorization-method');
             assert.equal(authorizationMethod.getAttribute('type'), 'pass through');
           });
