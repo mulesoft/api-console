a54f98b496d1da2fcb336c2748dbb93f9e98c25a
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2022-09-16 15:59:14 -0400

    test: add test for multiple messages in operation

--- test/api-console-documentation.test.js
+++ test/api-console-documentation.test.js
@@ -954,4 +954,32 @@ describe('API Console documentation', () => {
       });
     });
   });
+
+  // AsyncAPI
+  [
+    new ApiDescribe('Regular model'),
+    new ApiDescribe('Compact model', true)
+  ].forEach(({ label, compact }) => {
+    describe(label, () => {
+      const multipleMessagesApi = 'multiple-messages';
+
+      before(async () => {
+        amf = await AmfLoader.load({ compact, fileName: multipleMessagesApi });
+      });
+
+      describe('OAS 3.0', () => {
+        beforeEach(async () => {
+          element = await amfFixture(amf);
+          await navigationSelectEndpointMethod(element, 'shipping-messages', 'publish');
+          await nextFrame();
+        });
+
+        it('should render the messages dropdown selector', async () => {
+          await waitUntil(() => !!documentationMethod(element));
+          const methodDocumentation = documentationMethod(element);
+          assert.exists(methodDocumentation.shadowRoot.querySelector('.messages-options'));
+        });
+      });
+    });
+  });
 });
