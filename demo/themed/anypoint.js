c12b8739752a98838fab44095044c0ca1988a101
Alex Perez <alexperez@mulesoft.com>
2026-01-27 16:14:23 -0300

    feat: add gRPC API to the application and update API checks in ApiConsoleApp

--- demo/themed/anypoint.js
+++ demo/themed/anypoint.js
@@ -13,6 +13,7 @@ class ApicApplication extends DemoBase {
   constructor() {
     super();
     this.apis = [
+      ['grpc-test', 'gRPC API'],
       ['google-drive-api', 'Google Drive API'],
       ['httpbin', 'HTTPbin API'],
       ['data-type-fragment', 'RAML data type fragment'],
